9ed63b6712f4383c7ba1032e6d10d7053382fc3b
mamzyx <mamzyx@outlook.com>
2023-03-14 23:16:44 -0700

    frontpage-slider.php create

---
+++ templates/frontpage-slider.php
@@ -0,0 +1,53 @@
+<!--components slider start-->
+<section class="section-gap bg-extra pb-0">
+    <div class="neg-mt-300 landing-slider">
+        <div class="owl-carousel owl-theme dot-style-1" data-items="[2,2,2]" data-margin="30" data-autoplay="true" data-center="true" data-loop="true" data-dots="true">
+            <div class="item">
+                <img class="rounded" src="<?php get_template_directory_uri();?>/assets/img/thumb/banner-slider/1.jpg" alt="" />
+            </div>
+            <div class="item">
+                <img class="rounded" src="<?php get_template_directory_uri();?>/assets/img/thumb/banner-slider/2.jpg" alt="" />
+            </div>
+            <div class="item">
+                <img class="rounded" src="<?php get_template_directory_uri();?>/assets/img/thumb/banner-slider/3.jpg" alt="" />
+            </div>
+            <div class="item">
+                <img class="rounded" src="<?php get_template_directory_uri();?>/assets/img/thumb/banner-slider/4.jpg" alt="" />
+            </div>
+            <div class="item">
+                <img class="rounded" src="<?php get_template_directory_uri();?>/assets/img/thumb/banner-slider/5.jpg" alt="" />
+            </div>
+            <div class="item">
+                <img class="rounded" src="<?php get_template_directory_uri();?>/assets/img/thumb/banner-slider/6.jpg" alt="" />
+            </div>
+            <div class="item">
+                <img class="rounded" src="<?php get_template_directory_uri();?>/assets/img/thumb/banner-slider/7.jpg" alt="" />
+            </div>
+            <div class="item">
+                <img class="rounded" src="<?php get_template_directory_uri();?>/assets/img/thumb/banner-slider/8.jpg" alt="" />
+            </div>
+            <div class="item">
+                <img class="rounded" src="<?php get_template_directory_uri();?>/assets/img/thumb/banner-slider/9.jpg" alt="" />
+            </div>
+            <div class="item">
+                <img class="rounded" src="<?php get_template_directory_uri();?>/assets/img/thumb/banner-slider/10.jpg" alt="" />
+            </div>
+            <div class="item">
+                <img class="rounded" src="<?php get_template_directory_uri();?>/assets/img/thumb/banner-slider/11.jpg" alt="" />
+            </div>
+            <div class="item">
+                <img class="rounded" src="<?php get_template_directory_uri();?>/assets/img/thumb/banner-slider/12.jpg" alt="" />
+            </div>
+            <div class="item">
+                <img class="rounded" src="<?php get_template_directory_uri();?>/assets/img/thumb/banner-slider/13.jpg" alt="" />
+            </div>
+            <div class="item">
+                <img class="rounded" src="<?php get_template_directory_uri();?>/assets/img/thumb/banner-slider/14.jpg" alt="" />
+            </div>
+            <div class="item">
+                <img class="rounded" src="<?php get_template_directory_uri();?>/assets/img/thumb/banner-slider/15.jpg" alt="" />
+            </div>
+        </div>
+    </div>
+</section>
+<!--components slider end-->

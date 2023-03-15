b0b5ab384c20a8c1bbdf937a6a2e120edfa5d0c1
mamzyx <mamzyx@outlook.com>
2023-03-14 23:04:20 -0700

    add hero.php file

---
+++ templates/hero.php
@@ -0,0 +1,33 @@
+<!--hero start-->
+<section class="section-gap section-top- section-full clab-shape">
+    <div class="hero-img bg-gradient-overlay" data-overlay="9"></div>
+    <div class="container">
+        <div class="row justify-content-center align-items-center text-center text-white">
+            <div class="col-lg-10">
+                <!-- heading -->
+                <h1 class=" mb-4">
+                    بلوک و قالب مبتنی بر کامپوننت
+                </h1>
+
+                <!-- subheading -->
+                <p class="mb-lg-5 mb-4 lead">
+                    بیش از 180 عدد از قطعات و بلوک های پیش ساخته ساخته شده اند تا طراحی مدرن و مرسوم را تضمین کنند <br>
+                    برای پروژه های آینده شما.
+                </p>
+
+                <!-- button -->
+                <div class=" mb-0">
+                    <a href="#" class="btn btn-pill btn-solid-light mb-2">
+                        <i class="fa fa-play pr-3"></i>
+                        نمایش ویدیو موفقیت
+                    </a>
+
+                    <a href="#demo" class="btn btn-pill btn-outline-light ml-3 mb-2">
+                        شروع به کاوش کنید
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--hero end-->
